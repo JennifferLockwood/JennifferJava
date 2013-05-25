29c8ec88a9dd9dc8a62ba28ff418fba161c95aca
JennifferLockwood <jennifferlockwood@gmail.com>
2013-05-25 10:44:39 -0700

    Work done so far in Java

---
+++ Hello2/src/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Johnniffer
+ * Date: 5/23/13
+ * Time: 6:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hello {
+    public static void main(String args[]) {
+        if (args.length > 0)
+            System.out.println("Hello, " + args[0]);
+        System.out.println("Hello World!");
+    }
+}
