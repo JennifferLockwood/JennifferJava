8f57e983ae47438f98b95dc79044d464acb1a7fb
JennifferLockwood <jennifferlockwood@gmail.com>
2013-05-29 07:24:03 -0700

    20 gallons calculation

--- GallonsLiters/src/GalToLit.java
+++ GallonsLiters/src/GalToLit.java
@@ -15,7 +15,7 @@ public class GalToLit {
         double gallons; // holds the number of gallons
         double liters; // holds the number of liters
 
-        gallons = 10; // start with 10 gallons
+        gallons = 20; // start with 10 gallons
 
         liters = gallons * 3.7854; // convert to liters
 
