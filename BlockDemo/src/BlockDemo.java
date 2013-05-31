7456f0a68c57941c61043e0b194f40bcc6ad5e53
JennifferLockwood <jennifferlockwood@gmail.com>
2013-05-30 17:26:20 -0700

    Chane print by println

---
+++ BlockDemo/src/BlockDemo.java
@@ -0,0 +1,24 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Johnniffer
+ * Date: 5/30/13
+ * Time: 5:07 PM
+ * To change this template use File | Settings | File Templates.
+ * Demonstrate a block code.
+ * Call this file BlockDemo.java.
+ */
+public class BlockDemo {
+    public static void main(String args[]) {
+        double i, j, d;
+
+        i = 5;
+        j = 10;
+
+        // the target of this if is a blovk
+        if(i != 0) {
+            System.out.println("i does no equal to zero");
+            d = j / i;
+            System.out.println("j / i is " + d);
+        }
+    }
+}
