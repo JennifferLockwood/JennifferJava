a796b6b9c41419ba9c27f3d118e07602a75380f3
JennifferLockwood <jennifferlockwood@gmail.com>
2013-06-04 16:42:44 -0700

    Characters x, y, z

---
+++ CharInte/src/CharInte.java
@@ -0,0 +1,22 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Johnniffer
+ * Date: 6/4/13
+ * Time: 4:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+// Character variables can be handled like integers.
+public class CharInte {
+    public static void main(String args[]) {
+        char ch;
+
+        ch = 'X';
+        System.out.println("ch contains " + ch);
+
+        ch++; //increment ch
+        System.out.println("ch is now " + ch);
+
+        ch = 90; // give ch the value z
+        System.out.println("ch is now " + ch);
+    }
+}
