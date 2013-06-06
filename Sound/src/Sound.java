58233d01c2ce2cbe7d116f2900e5c90662202a62
JennifferLockwood <jennifferlockwood@gmail.com>
2013-06-05 17:25:57 -0700

    7.2 seconds

---
+++ Sound/src/Sound.java
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Johnniffer
+ * Date: 6/5/13
+ * Time: 5:17 PM
+ * To change this template use File | Settings | File Templates.
+ * This program computes the distance to a lighting strike whose
+ * sound takes 7.2 seconds to reach you.
+ */
+public class Sound {
+    public static void main(String args[]) {
+        double dist;
+
+        dist = 7.2 * 1100;
+
+        System.out.println("The lighting is " + dist + " feet away.");
+    }
+}
