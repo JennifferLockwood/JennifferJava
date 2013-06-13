d5b005fcd848496d774a5277d53d6ea8f9ab7c2b
John Lockwood <elitepropertiesbroker@gmail.com>
2013-06-13 14:43:15 -0700

    Table of two columns

---
+++ LogicalOpTable/src/LogicalOpTable.java
@@ -0,0 +1,31 @@
+// This Try  2-2: a truth table for the logical operators.
+
+class LogicalOpTable {
+    public static void main(String args[]) {
+
+        boolean p, q;
+
+        System.out.println("P\tQ\tAND\tOR\tXOR\tNOT");
+
+        p = true; q = true;
+        System.out.println(p + "\t" + q + "\t");
+        System.out.println((p & q) + "\t" + (p | q) + "\t");
+        System.out.println((p ^ q) + "\t" + (!p));
+
+        p = true; q = false;
+        System.out.println(p + "\t" + q + "\t");
+        System.out.println((p & q) + "\t" + (p | q) + "\t");
+        System.out.println((p ^ q) + "\t" + (!p));
+
+        p = false; q = true;
+        System.out.println(p + "\t" + q + "\t");
+        System.out.println((p & q) + "\t" + (p | q) + "\t");
+        System.out.println((p ^ q) + "\t" + (!p));
+
+        p = false; q = false;
+        System.out.println(p + "\t" + q + "\t");
+        System.out.println((p & q) + "\t" + (p | q) + "\t");
+        System.out.println((p ^ q) + "\t" + (!p));
+
+    }
+}
