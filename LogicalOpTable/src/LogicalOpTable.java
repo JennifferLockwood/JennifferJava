81c9dc21aa2030be7f37523b14a559da574bb46a
John Lockwood <elitepropertiesbroker@gmail.com>
2013-06-13 14:48:56 -0700

    Table corrected with right header

--- LogicalOpTable/src/LogicalOpTable.java
+++ LogicalOpTable/src/LogicalOpTable.java
@@ -5,26 +5,26 @@ class LogicalOpTable {
 
         boolean p, q;
 
-        System.out.println("P\tQ\tAND\tOR\tXOR\tNOT");
+        System.out.println("P\t\tQ\t\tAND\t\tOR\t\tXOR\t\tNOT");
 
         p = true; q = true;
-        System.out.println(p + "\t" + q + "\t");
-        System.out.println((p & q) + "\t" + (p | q) + "\t");
+        System.out.print(p + "\t" + q + "\t");
+        System.out.print((p & q) + "\t" + (p | q) + "\t");
         System.out.println((p ^ q) + "\t" + (!p));
 
         p = true; q = false;
-        System.out.println(p + "\t" + q + "\t");
-        System.out.println((p & q) + "\t" + (p | q) + "\t");
+        System.out.print(p + "\t" + q + "\t");
+        System.out.print((p & q) + "\t" + (p | q) + "\t");
         System.out.println((p ^ q) + "\t" + (!p));
 
         p = false; q = true;
-        System.out.println(p + "\t" + q + "\t");
-        System.out.println((p & q) + "\t" + (p | q) + "\t");
+        System.out.print(p + "\t" + q + "\t");
+        System.out.print((p & q) + "\t" + (p | q) + "\t");
         System.out.println((p ^ q) + "\t" + (!p));
 
         p = false; q = false;
-        System.out.println(p + "\t" + q + "\t");
-        System.out.println((p & q) + "\t" + (p | q) + "\t");
+        System.out.print(p + "\t" + q + "\t");
+        System.out.print((p & q) + "\t" + (p | q) + "\t");
         System.out.println((p ^ q) + "\t" + (!p));
 
     }
