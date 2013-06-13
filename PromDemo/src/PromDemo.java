22a62d7ad7722f860f7b613f26b5c8b3d5632d7a
John Lockwood <elitepropertiesbroker@gmail.com>
2013-06-13 15:39:36 -0700

    Examples cast needed and np needed

---
+++ PromDemo/src/PromDemo.java
@@ -0,0 +1,16 @@
+// A promotion surprise!
+
+class PromDemo {
+    public static void main(String args[]) {
+        byte b;
+        int i;
+
+        b = 10;
+        i = b * b; // OK, no cast needed because result is already elevated to int.
+
+        b =10;
+        b = (byte) (b * b); // Cast is needed here to assign an int to a byte!
+
+        System.out.println("i and b: " + i + " " + b);
+    }
+}
