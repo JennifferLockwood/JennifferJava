14f52716f9da5eda9cddf7e16814beb15b175236
John Lockwood <elitepropertiesbroker@gmail.com>
2013-06-12 18:44:01 -0700

    Double is bigger than Long

---
+++ LongtoDouble/src/LongtoDouble.java
@@ -0,0 +1,14 @@
+// Demonstrate automatic conversion from long to double.
+
+class LongtoDouble {
+    public static void main(String args[]) {
+        long L;
+        double D;
+
+        L = 100123285L;
+        D = L;
+
+        System.out.println("L and D: " + L + " " + D);
+
+    }
+}
