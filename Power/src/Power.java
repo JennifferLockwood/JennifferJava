7861657ac7cf2b4b114a1af39a3cc2760309aa35
John Lockwood <elitepropertiesbroker@gmail.com>
2013-06-25 18:10:36 -0700

    Correct errors

--- Power/src/Power.java
+++ Power/src/Power.java
@@ -6,7 +6,7 @@ class Power {
         int result;
 
         for (int i = 0; i < 10; i++) {
-            result = i;
+            result = 1;
             e = i;
             while (e > 0) {
                 result *= 2;
